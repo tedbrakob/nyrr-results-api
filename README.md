e10cc14d5ffef5ff0ef11c761822b6ebd6f971c0
Ted Brakob <tedbrakob@gmail.com>
2023-03-04 21:47:56 -0800

    update readme, formatting, add postToNyrr function

--- README.md
+++ README.md
@@ -13,25 +13,9 @@ import NyrrApi from "nyrr-results-api";
 ...
 ...
 
-//more on token below
-const nyrrApi = new NyrrApi(token);
-
 //get team results for the Men's "A" division in 2022
-const divisionResults = await nyrrApi.getDivisionResults("AM", 2022);
+const divisionResults = await NyrrApi.getDivisionResults("AM", 2022);
 ```
-
-### Token
-The api at results.nyrr.org/api requires a token.  This token is not issued on a per-app basis, there is only one token.  For the time I've been working on this api wrapper it has not changed from below:
-
-`898d6b6aef0e4887`
-
-I've included the static `getToken()` function to get an up-to-date token.  This won't work from the frontend due to the CORS policy on the resource the token in retrieved from.  This function must be called from your own backend and passed along to the frontend.
-
-```
-  const token = await NyrrApi.getToken();
-```
-
-Once the token is obtained, pass that as a parameter to the constructor.
 
 ### Available functions
 All functions of `NyrrApi` return a promise that resolves to a value described below:
--- src/index.ts
+++ src/index.ts
@@ -2,46 +2,46 @@ import * as clubStandings from "./clubStandings";
 import * as awards from "./awards";
 import * as events from "./events";
 
-export default class NyrrApi {
-  async getYears() : clubStandings.getYears {
+export default {
+  async getYears(): clubStandings.getYears {
     return await clubStandings.getYears();
-  }
+  },
 
-  async getDivisionsResults (year:number) : clubStandings.getDivisionsResults {
+  async getDivisionsResults (year:number): clubStandings.getDivisionsResults {
     return await clubStandings.getDivisionsResults(year);
-  }
+  },
 
-  async getDivisionResults (divisionCode:string, year:number) : clubStandings.getDivisionResults {
+  async getDivisionResults (divisionCode:string, year:number): clubStandings.getDivisionResults {
     return await clubStandings.getDivisionResults(divisionCode, year);
-  }
+  },
 
-  async getTeams (year:number) : clubStandings.getTeams {
+  async getTeams (year:number): clubStandings.getTeams {
     return await clubStandings.getTeams(year);
-  }
+  },
 
   async getTeamAwards (
     eventCode:string, 
     teamCode:string, 
     gender:string | null = null, 
     minimumAge:number | null = null
-  ) : awards.getTeamAwards {
+  ): awards.getTeamAwards {
     return await awards.getTeamAwards(eventCode, teamCode, gender, minimumAge);
-  }
+  },
 
   async getTeamAwardRunners (
     eventCode:string, 
     teamCode:string, 
     teamGender:string | null = null, 
     teamMinimumAge:number | null = null
-  ) : awards.getTeamAwardRunners {
+  ): awards.getTeamAwardRunners {
     return await awards.getTeamAwardRunners(eventCode, teamCode, teamGender, teamMinimumAge);
-  }
+  },
 
   async eventsSearch (
     year: number | null = null,
     searchString:string = "",
     distance: string | null = null,
-  ) : events.search {
+  ): events.search {
     return await events.search(year, searchString, distance);
-  }
+  },
 }
